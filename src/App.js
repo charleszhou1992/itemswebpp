a43aaab09deb3cdc4bb3342a4582bbeb617d1392
周士祺 <zhoushiqi@zhoushiqideMacBook-Pro.local>
2020-09-03 23:41:18 +0900

    shop page and collection preview pages setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import HomePage from "./pages/homepage";
-import HatsPage from "./pages/hatspage"
-import {Route, Switch} from "react-router-dom"
 
+import ShopPage from "../src/pages/shop/shoppage";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/shop/hats" component={HatsPage} />
-    </Switch>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
--- src/pages/hatspage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const HatsPage = () => (
-    <h1>HATS</h1>
-);
-
-export default HatsPage;
